e613180fe3e58beea33575f8595c99a3c2eb1191
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-30 15:58:36 -0300

    estilizacao e funcionamento do Ordenador

--- aluroni/src/pages/Cardapio/index.tsx
+++ aluroni/src/pages/Cardapio/index.tsx
@@ -3,10 +3,12 @@ import { ReactComponent as Logo } from 'assets/logo.svg'
 import Buscador from './Buscador';
 import { useState } from 'react';
 import Filtros from './Filtros';
+import Ordenador from './Ordenador';
 
 export default function Cardapio() {
     const [busca, setBusca] = useState("");
     const [filtro, setFiltro] = useState<number | null>(null);
+    const [ordenador, setOrdenador] = useState("");
     return (
         <main>
             <nav className={styles.menu}>
@@ -24,6 +26,7 @@ export default function Cardapio() {
                 />
                 <div className={styles.cardapio__filtros}>
                     <Filtros filtro={filtro} setFiltro={setFiltro} />
+                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
                 </div>
             </section>
         </main>
