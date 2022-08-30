89a9e025d26d99944115c5c1204eca4b815aa36c
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-30 14:41:23 -0300

    criacao e estilizacao dos filtros de pesquisa

--- aluroni/src/pages/Cardapio/index.tsx
+++ aluroni/src/pages/Cardapio/index.tsx
@@ -2,6 +2,7 @@ import styles from './Cardapio.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg'
 import Buscador from './Buscador';
 import { useState } from 'react';
+import Filtros from './Filtros';
 
 export default function Cardapio() {
     const [busca, setBusca] = useState("");
@@ -20,6 +21,9 @@ export default function Cardapio() {
                 <Buscador
                     busca={busca} setBusca={setBusca}
                 />
+                <div className={styles.cardapio__filtros}>
+                    <Filtros />
+                </div>
             </section>
         </main>
     );
