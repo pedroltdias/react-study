9cf9eb94bdeff15b63d6cd44b09a68689b83265a
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-31 14:49:33 -0300

    imagens importadas dinamicamente

--- aluroni/src/pages/Cardapio/Itens/Item/index.tsx
+++ aluroni/src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,15 +1,15 @@
 import styles from './Item.module.scss';
-import logo from 'assets/logo.svg';
 import cardapio from '../itens.json';
+import classNames from 'classnames';
 
 type Props = typeof cardapio[0];
 
 export default function Item(props: Props) {
-    const { title, description, category, serving, size, price } = props;
+    const { title, description, category, serving, size, price, photo } = props;
     return (
         <div className={styles.item}>
             <div className={styles.item__imagem}>
-                <img src={logo} alt={title} />
+                <img src={photo} alt={title} />
             </div>
             <div className={styles.item__descricao}>
                 <div className={styles.item__titulo}>
@@ -17,7 +17,14 @@ export default function Item(props: Props) {
                     <p>{description}</p>
                 </div>
                 <div className={styles.item__tags}>
-                    <div className={styles.item__tipo}>{category.label}</div>
+                    <div className={
+                        classNames(
+                            {
+                                [styles.item__tipo]: true,
+                                [styles[`item__tipo__${category.label.toLowerCase()}`]]: true,
+                            }
+                        )
+                    }>{category.label}</div>
                     <div className={styles.item__porcao}>{size}g</div>
                     <div className={styles.item__qtdpessoas}>Serve {serving} pessoa{serving === 1 ? '' : 's'}</div>
                     <div className={styles.item__valor}>R$ {price.toFixed(2)}</div>
