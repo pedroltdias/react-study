3432c3414e2cfb73866a23db388dbc4e80f79472
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-09-01 15:47:16 -0300

    utilizacao de nested routes

--- aluroni/src/pages/Cardapio/index.tsx
+++ aluroni/src/pages/Cardapio/index.tsx
@@ -4,34 +4,21 @@ import { useState } from "react";
 import Filtros from "./Filtros";
 import Ordenador from "./Ordenador";
 import Itens from "./Itens";
-import Menu from "components/Menu";
+import stylesTema from "styles/Tema.module.scss";
 
 export default function Cardapio() {
 	const [busca, setBusca] = useState("");
 	const [filtro, setFiltro] = useState<number | null>(null);
 	const [ordenador, setOrdenador] = useState("");
 	return (
-		<main>
-			<header className={styles.header}>
-				<div className={styles.header__text}>
-					A casa do código e da massa.
-				</div>
-			</header>
-			<section className={styles.cardapio}>
-				<h3 className={styles.cardapio__titulo}>Cardápio</h3>
-				<Buscador
-					busca={busca} setBusca={setBusca}
-				/>
-				<div className={styles.cardapio__filtros}>
-					<Filtros filtro={filtro} setFiltro={setFiltro} />
-					<Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
-				</div>
-				<Itens
-					busca={busca}
-					filtro={filtro}
-					ordenador={ordenador}
-				/>
-			</section>
-		</main>
+		<section className={styles.cardapio}>
+			<h3 className={stylesTema.titulo}>Cardápio</h3>
+			<Buscador busca={busca} setBusca={setBusca} />
+			<div className={styles.cardapio__filtros}>
+				<Filtros filtro={filtro} setFiltro={setFiltro} />
+				<Ordenador ordenador={ordenador} setOrdenador={setOrdenador} />
+			</div>
+			<Itens busca={busca} filtro={filtro} ordenador={ordenador} />
+		</section>
 	);
 }
