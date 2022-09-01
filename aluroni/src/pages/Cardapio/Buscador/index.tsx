86e7129da9e46fb900f916df9065c6b1550b02cc
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-09-01 13:32:11 -0300

    instalacao do eslint e utilizacao para corrigir erros de formatacao de codigo

--- aluroni/src/pages/Cardapio/Buscador/index.tsx
+++ aluroni/src/pages/Cardapio/Buscador/index.tsx
@@ -8,18 +8,18 @@ interface Props {
 }
 
 export default function Buscador({ busca, setBusca }: Props) {
-    return (
-        <div className={styles.buscador}>
-            <input
-                type="text"
-                value={busca}
-                onChange={(evento) => setBusca(evento.target.value)}
-                placeholder="Buscar"
-            />
-            <CgSearch
-                size={20}
-                color="#4C4D5E"
-            />
-        </div>
-    );
+	return (
+		<div className={styles.buscador}>
+			<input
+				type="text"
+				value={busca}
+				onChange={(evento) => setBusca(evento.target.value)}
+				placeholder="Buscar"
+			/>
+			<CgSearch
+				size={20}
+				color="#4C4D5E"
+			/>
+		</div>
+	);
 }
--- aluroni/src/pages/Cardapio/Filtros/index.tsx
+++ aluroni/src/pages/Cardapio/Filtros/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import filtros from './filtros.json';
-import styles from './Filtros.module.scss';
-import classNames from 'classnames';
+import React from "react";
+import filtros from "./filtros.json";
+import styles from "./Filtros.module.scss";
+import classNames from "classnames";
 
 type IOpcao = typeof filtros[0];
 
@@ -11,25 +11,25 @@ interface Props {
 }
 
 export default function Filtros({ filtro, setFiltro }: Props) {
-    function selecionarFiltro(opcao: IOpcao) {
-        if (filtro === opcao.id) return setFiltro(null);
-        return setFiltro(opcao.id);
-    }
+	function selecionarFiltro(opcao: IOpcao) {
+		if (filtro === opcao.id) return setFiltro(null);
+		return setFiltro(opcao.id);
+	}
 
-    return (
-        <div className={styles.filtros}>
-            {filtros.map((opcao) => (
-                <button
-                    className={classNames({
-                        [styles.filtros__filtro]: true,
-                        [styles['filtros__filtro--ativo']]: filtro === opcao.id
-                    })}
-                    key={opcao.id}
-                    onClick={() => selecionarFiltro(opcao)}
-                >
-                    {opcao.label}
-                </button>
-            ))}
-        </div>
-    );
+	return (
+		<div className={styles.filtros}>
+			{filtros.map((opcao) => (
+				<button
+					className={classNames({
+						[styles.filtros__filtro]: true,
+						[styles["filtros__filtro--ativo"]]: filtro === opcao.id
+					})}
+					key={opcao.id}
+					onClick={() => selecionarFiltro(opcao)}
+				>
+					{opcao.label}
+				</button>
+			))}
+		</div>
+	);
 }
