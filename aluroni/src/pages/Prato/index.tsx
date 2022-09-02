e7db64afbb431e65701ca57f600ff454324244a0
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-09-02 15:50:48 -0300

    exibindo o header apenas nos pratos existentes

--- aluroni/src/pages/Prato/index.tsx
+++ aluroni/src/pages/Prato/index.tsx
@@ -1,8 +1,9 @@
 import styles from "./Prato.module.scss";
-import { useNavigate, useParams } from "react-router-dom";
+import { Route, Routes, useNavigate, useParams } from "react-router-dom";
 import cardapio from "data/cardapio.json";
 import TagsPrato from "components/TagsPrato";
 import NotFound from "pages/NotFound";
+import PaginaPadrao from "components/PaginaPadrao";
 
 export default function Prato() {
 	const { id } = useParams();
@@ -12,24 +13,30 @@ export default function Prato() {
 		return <NotFound />;
 	}
 	return (
-		<>
-			<button className={styles.voltar} onClick={() => navigate(-1)}>
-				{"< Voltar"}
-			</button>
-			<section className={styles.container}>
-				<h1 className={styles.titulo}>
-					{prato.title}
-				</h1>
-				<div className={styles.imagem}>
-					<img src={prato.photo} alt={prato.title} />
-				</div>
-				<div className={styles.conteudo}>
-					<p className={styles.conteudo__descricao}>
-						{prato.description}
-					</p>
-					<TagsPrato {...prato} />
-				</div>
-			</section>
-		</>
+		<Routes>
+			<Route path="*" element={<PaginaPadrao />}>
+				<Route index element={
+					<>
+						<button className={styles.voltar} onClick={() => navigate(-1)}>
+							{"< Voltar"}
+						</button>
+						<section className={styles.container}>
+							<h1 className={styles.titulo}>
+								{prato.title}
+							</h1>
+							<div className={styles.imagem}>
+								<img src={prato.photo} alt={prato.title} />
+							</div>
+							<div className={styles.conteudo}>
+								<p className={styles.conteudo__descricao}>
+									{prato.description}
+								</p>
+								<TagsPrato {...prato} />
+							</div>
+						</section>
+					</>
+				} />
+			</Route>
+		</Routes>
 	);
 }
