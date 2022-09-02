c948c83ccfc8e5104b88712e3b2cf8b0425fef84
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-09-02 15:43:46 -0300

    tornando item do cardapio clicavel

--- aluroni/src/pages/Cardapio/Itens/Item/index.tsx
+++ aluroni/src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,13 +1,16 @@
 import styles from "./Item.module.scss";
 import { Prato } from "types/Prato";
 import TagsPrato from "components/TagsPrato";
+import { useNavigate } from "react-router-dom";
 
 type Props = Prato;
 
 export default function Item(props: Props) {
-	const { title, description, photo } = props;
+	const { id, title, description, photo } = props;
+	const navigate = useNavigate();
+
 	return (
-		<div className={styles.item}>
+		<div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
 			<div className={styles.item__imagem}>
 				<img src={photo} alt={title} />
 			</div>
