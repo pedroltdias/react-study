7c27c1b8109760728fb94b2229f7675e9249021e
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-09-05 16:42:13 -0300

    correcao na quebra de linha

--- aluroni/src/pages/Sobre/index.tsx
+++ aluroni/src/pages/Sobre/index.tsx
@@ -17,7 +17,7 @@ export default function Sobre() {
 						Nós do Aluroni oferecemos a vocês, nossos queridos clientes, a Massa Italiana Caseira mais saborosa e sofisticada de São Paulo! Prezamos pelos ingredientes tradicionais da culinária Italiana, frescos e de excelente qualidade para que sua experiência seja ainda mais intensa!
 					</p>
 					<p>
-						Também possuímos uma cardápio de carnes com muitas opções de acordo com o seu gosto!
+						Também possuímos uma cardápio de carnes com muitas opções de acordo com o seu gosto! 
 					</p>
 					<p>
 						Para acompanhar as massas italianas, Aluroni possui uma reserva de vinhos especiais, que harmonizam perfeitamente com o seu parto, seja carne ou massa!
