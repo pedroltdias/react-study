4b90a26e988ae9a7262ed50ff39e39750c6aecb9
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-09-15 15:37:41 -0300

    refatoracao do axios

--- alfood/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ alfood/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import http from "../../../http";
 import IRestaurante from "../../../interfaces/IRestaurante";
 
 export default function FormularioRestaurante() {
@@ -9,7 +9,7 @@ export default function FormularioRestaurante() {
 
 	useEffect(() => {
 		if (parametros.id) {
-			axios.get<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`)
+			http.get<IRestaurante>(`restaurantes/${parametros.id}/`)
 				.then(resposta => setNomeRestaurante(resposta.data.nome));
 		}
 	}, [parametros]);
@@ -20,14 +20,14 @@ export default function FormularioRestaurante() {
 		evento.preventDefault();
 
 		if (parametros.id) {
-			axios.put(`http://localhost:8000/api/v2/restaurantes/${parametros.id}/`, {
+			http.put(`restaurantes/${parametros.id}/`, {
 				nome: nomeRestaurante
 			})
 				.then(() => {
 					alert("Restaurante ATUALIZADO com sucesso")
 				})
 		} else {
-			axios.post("http://localhost:8000/api/v2/restaurantes/", {
+			http.post("restaurantes/", {
 				nome: nomeRestaurante
 			})
 				.then(() => {
