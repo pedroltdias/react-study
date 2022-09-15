4743e79be5230a9e1a830e7c13312cdce4a95884
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-09-15 15:32:27 -0300

    estilizacao da pagina de formulario utilizando o mui

--- alfood/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ alfood/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,4 +1,4 @@
-import { Button, TextField } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -38,14 +38,25 @@ export default function FormularioRestaurante() {
 	}
 
 	return (
-		<form onSubmit={aoSubmeterForm}>
-			<TextField
-				value={nomeRestaurante}
-				onChange={evento => setNomeRestaurante(evento.target.value)}
-				id="standard-basic"
-				label="Nome do Restaurante"
-				variant="standard" />
-			<Button type="submit" variant="outlined">Salvar</Button>
-		</form>
+		<Box sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+			<Typography component="h1" variant="h6">Formulário de Restaurantes</Typography>
+			<Box component="form" onSubmit={aoSubmeterForm}>
+				<TextField
+					value={nomeRestaurante}
+					onChange={evento => setNomeRestaurante(evento.target.value)}
+					id="standard-basic"
+					label="Nome do Restaurante"
+					variant="standard"
+					fullWidth
+					required
+				/>
+				<Button
+					sx={{ marginTop: 1 }}
+					type="submit"
+					variant="outlined"
+					fullWidth
+				>Salvar</Button>
+			</Box>
+		</Box>
 	);
 }
