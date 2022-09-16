dce5e15b757c43d75d253d49c32b0fb4f3b4401e
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-09-16 16:05:15 -0300

    criacao de um layout base para a pagina administrativa

---
+++ alfood/src/paginas/Administracao/PaginaBaseAdmin.tsx
@@ -0,0 +1,40 @@
+import { AppBar, Box, Button, Container, Link, Paper, Toolbar, Typography } from "@mui/material";
+
+import { Link as RouterLink, Outlet } from "react-router-dom";
+
+export default function PaginaBaseAdmin() {
+	return (
+		<>
+			<AppBar position="static">
+				<Container maxWidth="xl">
+					<Toolbar>
+						<Typography variant="h6">
+							Administração
+						</Typography>
+						<Box sx={{ display: "flex", flexGrow: 1 }}>
+							<Link component={RouterLink} to="/admin/restaurantes/">
+								<Button sx={{ my: 2, color: 'white' }}>
+									Restaurantes
+								</Button>
+							</Link>
+							<Link component={RouterLink} to="/admin/restaurantes/novo/">
+								<Button sx={{ my: 2, color: 'white' }}>
+									Novo Restaurante
+								</Button>
+							</Link>
+						</Box>
+					</Toolbar>
+				</Container>
+			</AppBar>
+
+			<Box>
+				<Container maxWidth="lg" sx={{ mt: 1 }}>
+					<Paper sx={{ p: 2 }}>
+						<Outlet/>
+					</Paper>
+				</Container>
+			</Box>
+
+		</>
+	);
+}
--- alfood/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
+++ alfood/src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -1,10 +1,9 @@
-import { AppBar, Box, Button, Container, Link, Paper, TextField, Toolbar, Typography } from "@mui/material";
+import { Box, Button, Container, Paper, TextField, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import http from "../../../http";
 import IRestaurante from "../../../interfaces/IRestaurante";
 
-import { Link as RouterLink } from "react-router-dom";
 
 export default function FormularioRestaurante() {
 	const parametros = useParams();
@@ -40,56 +39,31 @@ export default function FormularioRestaurante() {
 	}
 
 	return (
-		<>
-			<AppBar position="static">
-				<Container maxWidth="xl">
-					<Toolbar>
-						<Typography variant="h6">
-							Administração
-						</Typography>
-						<Box sx={{ display: "flex", flexGrow: 1 }}>
-							<Link component={RouterLink} to="/admin/restaurantes/">
-								<Button sx={{ my: 2, color: 'white' }}>
-									Restaurantes
-								</Button>
-							</Link>
-							<Link component={RouterLink} to="/admin/restaurantes/novo/">
-								<Button sx={{ my: 2, color: 'white' }}>
-									Novo Restaurante
-								</Button>
-							</Link>
+		<Box>
+			<Container maxWidth="lg" sx={{ mt: 1 }}>
+				<Paper sx={{ p: 2 }}>
+					<Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", flexGrow: 1 }}>
+						<Typography component="h1" variant="h6">Formulário de Restaurantes</Typography>
+						<Box component="form" sx={{ width: "100%" }} onSubmit={aoSubmeterForm}>
+							<TextField
+								value={nomeRestaurante}
+								onChange={evento => setNomeRestaurante(evento.target.value)}
+								id="standard-basic"
+								label="Nome do Restaurante"
+								variant="standard"
+								fullWidth
+								required
+							/>
+							<Button
+								sx={{ marginTop: 1 }}
+								type="submit"
+								variant="outlined"
+								fullWidth
+							>Salvar</Button>
 						</Box>
-					</Toolbar>
-				</Container>
-			</AppBar>
-
-			<Box>
-				<Container maxWidth="lg" sx={{ mt: 1 }}>
-					<Paper sx={{ p: 2 }}>
-						<Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", flexGrow: 1 }}>
-							<Typography component="h1" variant="h6">Formulário de Restaurantes</Typography>
-							<Box component="form" sx={{ width: "100%" }} onSubmit={aoSubmeterForm}>
-								<TextField
-									value={nomeRestaurante}
-									onChange={evento => setNomeRestaurante(evento.target.value)}
-									id="standard-basic"
-									label="Nome do Restaurante"
-									variant="standard"
-									fullWidth
-									required
-								/>
-								<Button
-									sx={{ marginTop: 1 }}
-									type="submit"
-									variant="outlined"
-									fullWidth
-								>Salvar</Button>
-							</Box>
-						</Box>
-					</Paper>
-				</Container>
-			</Box>
-
-		</>
+					</Box>
+				</Paper>
+			</Container>
+		</Box>
 	);
 }
